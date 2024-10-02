1809d77751807d990617c5824bcdb72cee1b01d1
VINOD KUMAR SHARMA <vksharmaratanpura@gmail.com>
2024-10-02 14:56:13 +0530

    Update passwd.js

--- passwd.js
+++ passwd.js
@@ -1,12 +1,1 @@
-function authenticate()
-{
-    var x=document.forms["psswdform"]["pss"].value;
-    if(x=='Aditya' || x=='kuldeep' || x=='kunal' || x==1)
-    {
-        return true;
-    }
-    else{
-        window.alert("Enter correct password");
-        return false;
-    }
-}
+
